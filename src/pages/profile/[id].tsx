6229bc4971a05bbefe6b783586332aa83677bfa1
Ben <ncor89b@gmail.com>
2023-01-13 19:34:44 +0100

    data displaying profile completed

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -1,5 +1,5 @@
+import { MediaRenderer } from '@thirdweb-dev/react';
 import { useRouter } from 'next/router';
-import React from 'react';
 import { useProfileQuery, usePublicationsQuery } from '../../graphql/generated';
 import styles from "../../styles/Profile.module.css";
 
@@ -54,20 +54,51 @@ export default function ProfilePage({}: Props) {
   if (loadingProfile) {
     return <div>Loading Profile...</div>
   }
-  
 
   return (
     <div className={styles.profileContainer}>
-
       <div className={styles.profileContentContainer}>
 
         {/* Cover Image */}
+        {/* @ts-ignore */}
+        {profileData?.profile?.coverPicture?.original?.url && (
+        <MediaRenderer 
+        // @ts-ignore
+          src={profileData?.profile?.coverPicture?.original?.url || "https://res.cloudinary.com/dwoifuutn/image/upload/v1666536960/bored-landing-page_jgaklg.png"}
+          alt={profileData?.profile?.name || profileData?.profile?.handle || ""}
+          className={styles.coverImageContainer}
+        />
+        )}
 
         {/* Profile Picture */}
+        {/* @ts-ignore */}
+        {profileData?.profile?.picture.original.url && (
+            <MediaRenderer
+            // @ts-ignore
+              src={profileData.profile.picture.original.url || "https://res.cloudinary.com/dwoifuutn/image/upload/v1666343939/chat-dapp-logo_e9wmil.png"} 
+              alt={profileData.profile.name || profileData.profile.handle || ""}
+              className={styles.profilePictureContainer}
+            />
+        )} 
+
+        {/* Profile Name */}
+        <h2 className={styles.profileName}>
+          {profileData?.profile?.name || "User not exist"}
+        </h2>
 
         {/* Profile Handle */}
+        <p className={styles.profileHandle}>
+          {profileData?.profile?.handle || "User not exist"}
+        </p>
 
         {/* Profile Description */}
+        <p className={styles.profileDescription}>
+          {profileData?.profile?.bio}
+        </p>
+
+        <p className={styles.followerCount}>
+          {profileData?.profile?.stats.totalFollowers} {" Followers"}
+        </p>
       </div>
 
       <div className={styles.publicationsContainer}>
